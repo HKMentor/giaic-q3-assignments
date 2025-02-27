8076aeef7eaad90952cc21edba19d5a995655be6
Hooria_Fatima <160629586+HKMentor@users.noreply.github.com>
2025-02-28 02:31:14 +0500

    Add files via upload

---
+++ python-dataTypes/main.py
@@ -0,0 +1,86 @@
+# Python Data Types
+"""
+ Python has different data types for storing values. 
+ Strings (str) store text, integers (int) and floats (float) store numbers,
+ and booleans (bool) represent True/False. Lists (list) and tuples (tuple) hold multiple items, 
+ but lists are changeable, while tuples are fixed. Dictionaries (dict) store key-value pairs, 
+ sets (set) hold unique values, and NoneType (None) represents empty data.
+
+"""
+# String (str)
+print("STRING TYPE ")
+
+my_Variable = "Hooria fatima"
+print(my_Variable)
+
+Greet = "Moiz"
+print("How are you ?", Greet )
+
+# Integer (int)
+print("INTEGERS TYPE")
+
+age = 16
+print(age)
+
+a = 2
+b = 5
+my_Sum =(a + b)
+print("Sum is =", my_Sum)
+
+
+# Float (float)
+print("FLOAT TYPE")
+
+price = 99.99
+temperature = 24.6
+pi = 3.14
+
+print(price)
+print(temperature)
+print(pi)
+
+
+
+# Boolean (bool)
+print("BOOLEAN TYPE")
+
+is_student = True
+print(is_student)
+
+is_Married = False
+print(is_Married)
+
+# List (list)
+print("LIST TYPE")
+
+colors = ["red", "blue", "black", "pink"]
+print(colors)
+
+# Set (set)
+print("SET TYPE")
+
+numbers = {1,2,3,4,5,6,}
+print(numbers)
+
+# None (none)
+print("NONE TYPE")
+
+z = None
+print(z)
+
+name = None
+print("My name ", name)
+
+# Tuples (tuple)
+print('TUPLE TYPE')
+
+Days = ("mon", "tues", "wed", "thurs", "fri", "sat", "sun" )
+print(Days)
+
+# Dictionaries (dic)
+print("DICTIONARY TYPE")
+
+student = {"name": "Ali", "age": 20, "grade": "A"}  
+print(student)
+print(student["name"]) 
+
